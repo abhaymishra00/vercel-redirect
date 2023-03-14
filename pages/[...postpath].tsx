7927ac04a51aec1e4f987441d41ce93eeb74f1fe
Abhay Mishra <abhaymishrahello@gmail.com>
2023-03-14 23:59:48 +0530

    blank title

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,6 +82,7 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
+				<meta property="og:title" content={} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
@@ -95,9 +96,10 @@ const Post: React.FC<PostProps> = (props) => {
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
 				/>
+				<title>{}</title>
 			</Head>
 			<div className="post-container">
-		
+
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.altText || post.title}
